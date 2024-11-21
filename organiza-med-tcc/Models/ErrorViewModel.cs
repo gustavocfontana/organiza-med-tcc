6f587ccfb42f65c6e60dfa84841f694cb36ce836
Gustavo Fontana <gucfontana@gmail.com>
2024-11-21 15:38:02 -0300

    Add project files.

---
+++ organiza-med-tcc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace organiza_med_tcc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
