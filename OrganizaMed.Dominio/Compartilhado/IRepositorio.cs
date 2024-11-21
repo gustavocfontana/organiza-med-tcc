cb39c42348ea29ac2666ccf2a0ae1012cde07454
Gustavo Fontana <gucfontana@gmail.com>
2024-11-21 16:24:58 -0300

    arquivos iniciais dominio

---
+++ OrganizaMed.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganizaMed.Dominio.Compartilhado
+{
+    public interface IRepositorio<T> where T : EntidadeBase
+    {
+        T ObterPorId(int id);
+        List<T> SelecionarTodos();
+        void Adicionar(T entidade);
+        void Atualizar(T entidade);
+        void Remover(T entidade);
+    }
+}
