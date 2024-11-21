0c088e3afe881a9f658928b07e141769c6c26f3a
Gustavo Fontana <gucfontana@gmail.com>
2024-11-21 16:41:35 -0300

    arquivos iniciais infra

---
+++ OrganizaMed.Infra/Compartilhado/OrganizaMedDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace OrganizaMed.Infra.Compartilhado
+{
+    public class OrganizaMedDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("SqlServer");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ OrganizaMed.Infra/Compartilhado/RepositorioBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrganizaMed.Dominio.Compartilhado;
+
+namespace OrganizaMed.Infra.Compartilhado
+{
+    public abstract class RepositorioBase<T> where T : EntidadeBase
+    {
+        protected readonly OrganizaMedDbContext dbContext;
+
+        public RepositorioBase(OrganizaMedDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected abstract DbSet<T> ObterRegistros();
+
+        public void Adicionar(T entidade)
+        {
+            ObterRegistros().Add(entidade);
+            dbContext.SaveChanges();
+        }
+
+        public void Atualizar(T entidade)
+        {
+            ObterRegistros().Update(entidade);
+            dbContext.SaveChanges();
+        }
+
+        public void Remover(T entidade)
+        {
+            ObterRegistros().Remove(entidade);
+            dbContext.SaveChanges();
+        }
+
+        public virtual T ? ObterPorId(int id)
+        {
+            return ObterRegistros()
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public virtual List<T> ObterTodos()
+        {
+            return ObterRegistros()
+                .ToList();
+        }
+    }
+}
