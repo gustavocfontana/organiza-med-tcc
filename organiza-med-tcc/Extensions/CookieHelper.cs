5b5a43b5249cf89d62a4c2195e47cf40d4da0a62
Gustavo Fontana <gucfontana@gmail.com>
2024-11-26 16:11:49 -0300

    adicionado dark-mode + cookiehelper

---
+++ organiza-med-tcc/Extensions/CookieHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+public static class CookieHelper
+{
+    public static void SetCookie(HttpContext context, string key, string value, int ? expireTime)
+    {
+        CookieOptions option = new CookieOptions();
+
+        if (expireTime.HasValue)
+            option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+        else
+            option.Expires = DateTime.Now.AddDays(7);
+
+        context.Response.Cookies.Append(key, value, option);
+    }
+
+    public static string GetCookie(HttpContext context, string key)
+    {
+        return context.Request.Cookies[key];
+    }
+}
